06b44651001d0682f49696137f38d7f07fff4fa3
chhavi2327 <145341670+chhavi2327@users.noreply.github.com>
2023-09-19 06:53:49 +0000

    practicecode

---
+++ firscode.js
@@ -0,0 +1,19 @@
+//primitive datatype
+//7 types string,boolean,null,undefined,symbol(used to make a value unique),bigInt,number
+
+//non primitive
+//arrays,object,function
+
+//decimal numbers are also comes under number datatype
+
+let score;
+console.log(typeof score);
+//datatype and there typeof
+//null=object
+//undefined=undefined
+//boollen=boollen
+//number=number
+//string=string
+//arary=object
+//function=function
+//object=object
